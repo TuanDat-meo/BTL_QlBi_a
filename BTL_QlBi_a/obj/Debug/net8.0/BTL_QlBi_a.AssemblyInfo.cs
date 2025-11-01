caf131fdb8dcd849e53a8b079f279cea17004767
đạt tuan <snsnj6179@gmail.com>
2025-11-01 13:32:31 +0700

    update-demo

--- BTL_QlBi_a/obj/Debug/net8.0/BTL_QlBi_a.AssemblyInfo.cs
+++ BTL_QlBi_a/obj/Debug/net8.0/BTL_QlBi_a.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8872c947db3b0caf1dad3b57dc40810b95f35e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f4c69dd3fb7b5d06e706289de8c40a4cf73cd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
