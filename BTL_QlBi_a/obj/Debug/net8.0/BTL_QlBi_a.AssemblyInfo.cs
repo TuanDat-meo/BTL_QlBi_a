60023dde2471683acafbcb8a69680d6db6a65edd
đạt tuan <snsnj6179@gmail.com>
2025-11-03 00:48:47 +0700

    fix_conflict

--- BTL_QlBi_a/obj/Debug/net8.0/BTL_QlBi_a.AssemblyInfo.cs
+++ BTL_QlBi_a/obj/Debug/net8.0/BTL_QlBi_a.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418e83249596f829121adcee216b170d29fd7be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5003d987f647970175566ecdd00a5bc114888af6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
