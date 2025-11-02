0f4c059352e5b6e95ed7a3587a0392165df62769
đạt tuan <snsnj6179@gmail.com>
2025-11-03 00:50:36 +0700

    fix_conflict1

--- BTL_QlBi_a/obj/Release/net8.0/BTL_QlBi_a.AssemblyInfo.cs
+++ BTL_QlBi_a/obj/Release/net8.0/BTL_QlBi_a.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc94aa276a9f5e44add15b6b0fccabbae7f7bbe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
