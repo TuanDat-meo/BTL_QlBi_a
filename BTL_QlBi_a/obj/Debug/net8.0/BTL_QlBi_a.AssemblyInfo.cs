1c4037fb4110cd39c438f30051f87d20b7609abe
đạt tuan <snsnj6179@gmail.com>
2025-11-06 16:28:32 +0700

    fix-conlict

--- BTL_QlBi_a/obj/Debug/net8.0/BTL_QlBi_a.AssemblyInfo.cs
+++ BTL_QlBi_a/obj/Debug/net8.0/BTL_QlBi_a.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f04d803a609e70fa3f2d88c7fc70c8c7528b1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b5eaeb730bf85d8fadc8be765092de420d4317")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_QlBi_a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
