c84d16d41c5465a16a4c5699358a6060c8de1733
binh-ff9 <duybinh7700@gmail.com>
2025-11-13 15:22:38 +0700

    them cay user

---
+++ BTL_QlBi_a/Controllers/Users/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using BTL_QlBi_a.Models.EF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_QlBi_a.Controllers.Users
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
