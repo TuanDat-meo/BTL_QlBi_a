06878b644c00173fb1286499049c69902dd0d9d4
binh-ff9 <duybinh7700@gmail.com>
2025-11-14 11:35:29 +0700

    Them hieu ung loading

--- BTL_QlBi_a/Controllers/Users/UsersController.cs
+++ BTL_QlBi_a/Controllers/Users/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using BTL_QlBi_a.Models.EF;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BTL_QlBi_a.Controllers.Users
 {
@@ -13,7 +14,38 @@ namespace BTL_QlBi_a.Controllers.Users
         }
         public async Task<IActionResult> Index()
         {
-            return View();
+            var dsBan = await _context.BanBia
+                .Include(b => b.LoaiBan)
+                .OrderBy(b => b.TenBan)
+                .ToListAsync();
+
+            return View(dsBan);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TraCuuNhanVien(string sdt)
+        {
+            if (string.IsNullOrEmpty(sdt))
+            {
+                return Json(new { success = false, message = "Vui lòng nhập SĐT" });
+            }
+            var kh = await _context.KhachHang
+                .FirstOrDefaultAsync(k => k.SDT == sdt && k.HoatDong == true);
+
+            if (kh == null) return Json(new { success = false, message = "Không tìm thấy thông tin khách hàng." });
+
+            return Json(new
+            {
+                success = true,
+                data = new
+                {
+                    ten = kh.TenKH,
+                    hang = kh.HangTV.ToString(),
+                    diem = kh.DiemTichLuy,
+                    avatar = kh.Avatar ?? "/asset/img/user.svg",
+                    chiTieu = kh.TongChiTieu.ToString("N0") + " đ"
+                }
+            });
         }
     }
 }
